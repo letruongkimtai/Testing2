f2a14e321992a61ded7218826c12b3908a722137
Kim Tài <letruongkimtai@gmail.com>
2018-12-19 21:41:02 +0700

    init

---
+++ WebApplication1/WebApplication1/Models/soemthing1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class soemthing1
+    {
+        public int soemthing1 { get; set; }
+    }
+}
